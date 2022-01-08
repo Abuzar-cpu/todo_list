c62a108e90f49e73a5a3f062cec72bc27f91b154
Abuzar-cpu <kerimovabuzer25@gmail.com>
2022-01-08 16:08:24 +0400

    now user can undo a task that was completed

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-let allNotes = [];
 let completed = [];
 let notCompleted = [];
-
+let allTabs = ["All notes", "Completed", "Not completed"];
+let activeTab = 0;
 
 let list = document.querySelector("#list");
 
+/*Adds new note as not completed*/
 function addNote() {
     if(this.document.querySelector("#userInput").value != "")
     {
-        allNotes.push(this.document.querySelector("#userInput").value);
         notCompleted.push(this.document.querySelector("#userInput").value);
         this.document.querySelector("#userInput").value = "";
-        // console.log(allNotes);
         
-        list.innerHTML = "";
-        for(let i = 0 ; i < allNotes.length; i++)
+        if(activeTab === 0)
         {
-            var a = document.createElement("li");
-            a.setAttribute("id", "item");
-            a.setAttribute("onClick", "markAsCompleted(this)")
-            a.innerText = allNotes[i];
-            list.append(a);
+            showAll();
+            return;
         }
+        else if(activeTab === 1)
+        {
+            showCompleted();
+            return;
+        }
+        
+        showNotCompleted();
+        return;
     }
-    showAll();
 }
+/**End of add note */
 
+/**Clears all notes
+ * NO EXCEPTION 
+ */
 function clearAll() {
     let answer = confirm("All notes will be deleted");
 
     if(answer)
     {
-        while(allNotes.length != 0)
-        {
-            allNotes.pop();
-        }
-
         while(completed.length != 0)
         {
             completed.pop();
@@ -48,14 +49,19 @@ function clearAll() {
         list.innerHTML = "";
     }
 }
+/**End of clear all */
 
+/**Shows all notes
+ * Order: not completed first, then completed tasks
+ */
 function showAll() {
     list.innerHTML = "";
     for(let i = notCompleted.length - 1 ; i >= 0; i--)
     {
         var a = document.createElement("li");
         a.setAttribute("id", "item");
-        a.setAttribute("onClick", "markAsCompleted(this)")
+        a.setAttribute("onClick", "markAsCompleted(this)");
+        a.classList.add("notCompleted");
         a.innerText = notCompleted[i];
         list.append(a);
     }
@@ -64,49 +70,88 @@ function showAll() {
     {
         var a = document.createElement("li");
         a.setAttribute("id", "item");
-        a.setAttribute("style", "text-decoration: line-through; background-color: grey; color: #fff;")
+        a.setAttribute("onClick", "markAsCompleted(this)");
+        a.classList.add("completed");
+        // a.setAttribute("style", "text-decoration: line-through; background-color: grey; color: #fff;")
         a.innerText = completed[i];
         list.append(a);
     }
 }
+/**End of show all */
 
+/**Show completed method
+ * Shows all completed tasks
+ * iterating through completed list of tasks
+ * works when user clicks on completed button
+ */
 function showCompleted() {
     list.innerHTML = "";
     for(let i = 0 ; i < completed.length; i++)
     {
         var a = document.createElement("li");
         a.setAttribute("id", "item");
-        a.setAttribute("style", "text-decoration: line-through; background-color: grey; color: #fff;")
+        a.setAttribute("onClick", "markAsCompleted(this)");
+        a.classList.add("completed");
+        // a.setAttribute("style", "text-decoration: line-through; background-color: grey; color: #fff;")
         a.innerText = completed[i];
         list.append(a);
     }
 }
+/**End of showCompleted */
 
+/**Show not completed method */
 function showNotCompleted() {
     list.innerHTML = "";
     for(let i = notCompleted.length - 1 ; i >= 0; i--)
     {
         var a = document.createElement("li");
         a.setAttribute("id", "item");
+        a.classList.add("notCompleted");
         a.setAttribute("onClick", "markAsCompleted(this)")
         a.innerText = notCompleted[i];
         list.append(a);
     }
 }
+/**End of show not completed */
 
+/**Mark as completed
+ * Currently under development
+ * changes the status of task
+ * and its appearance based on status
+ */
 function markAsCompleted(element) {
-    let completedNow = notCompleted.pop(element.innerText);
-    // console.log(completedNow);
-    if(completedNow != undefined)
+
+    let txt = element.innerText;
+    if(completed.indexOf(txt) != -1)
     {
-        completed.push(completedNow);
-        element.setAttribute("style", "text-decoration: line-through; background-color: grey; color: #fff;")
+        notCompleted.push(txt);
+        completed.splice(completed.indexOf(txt), 1);
     }
+    else
+    {
+        completed.push(txt);
+        notCompleted.splice(notCompleted.indexOf(txt), 1);
+    }
+    if(activeTab === 0)
+    {
+        showAll();
+        return;
+    }
+    else if(activeTab === 1)
+    {
+        showCompleted();
+        return;
+    }
+    
+    showNotCompleted();
+    return;
+
 }
+/**End of mark as completed */
 
-
-
-
+/**Change tab method
+ * Changes the appearance of active tab
+ */
 function changeTab(tab) {
     let tabs = document.getElementsByClassName("nav-link");
     for(let i = 0; i < tabs.length; i++)
@@ -115,50 +160,8 @@ function changeTab(tab) {
     }
 
     tab.setAttribute("style", "border: 1px solid red;");
-    // console.log(tabs); 
+    activeTab = allTabs.indexOf(tab.innerText);
 }
-
+/**End of change tab method */
 
 changeTab(document.getElementsByClassName("nav-link")[0]);
-
-//TODO: Complete this task using the code belows
-
-// function addNote() {
-//     if(this.document.querySelector("#userInput").value != "")
-//     {
-//         allNotes.push(this.document.querySelector("#userInput").value);
-//         this.document.querySelector("#userInput").value = "";
-//         // console.log(allNotes);
-        
-//         list.innerHTML = "";
-//         for(let i = 0 ; i < allNotes.length; i++)
-//         {
-//             var a = document.createElement("li");
-
-//             var cardBorder = document.createElement("div");
-//             cardBorder.setAttribute("class", "card border-primary mb-auto mt-5");
-//             cardBorder.setAttribute("style", "width: 50vw;");
-
-//             var cardHeader = document.createElement("div");
-//             cardHeader.setAttribute("class", "card-header");
-
-//             var cardBody = document.createElement("div");
-//             cardBody.setAttribute("class", "card-body text-primary");
-
-//             var h5 = document.createElement("h5");
-//             h5.setAttribute("class", "card-title");
-
-//             var p = document.createElement("p");
-//             h5.setAttribute("class", "card-text");
-
-//             cardBody.appendChild(p);
-//             cardBody.appendChild(h5);
-//             cardBorder.appendChild(cardHeader);
-//             cardBorder.appendChild(cardBody);
-
-//             a.append(cardBorder);
-//             a.innerText = allNotes[i];
-//             list.append(a);
-//         }
-//     }
-// }
